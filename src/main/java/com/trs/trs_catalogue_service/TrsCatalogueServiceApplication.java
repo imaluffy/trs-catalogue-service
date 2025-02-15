ab5ba4539a35f8eccc1de0a3e7109536df32b25d
imaluffy <msandesh258@gmail.com>
2025-02-16 01:40:49 +0530

    init catalogue service

---
+++ src/main/java/com/trs/trs_catalogue_service/TrsCatalogueServiceApplication.java
@@ -0,0 +1,15 @@
+package com.trs.trs_catalogue_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrsCatalogueServiceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TrsCatalogueServiceApplication.class, args);
+		System.out.println("Hello from Trs Catalogue");
+	}
+
+}
---
+++ src/test/java/com/trs/trs_catalogue_service/TrsCatalogueServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trs.trs_catalogue_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrsCatalogueServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
